94961afcfeb12c8938479c2c66f05d50bce3b1ed
WinstonBlack <35714947+WinstonBlack@users.noreply.github.com>
2018-01-22 20:42:50 -0800

    Create puzzle-8.js

---
+++ puzzle-8.js
@@ -0,0 +1,48 @@
+while (getColor != "red") {
+  down
+}
+var steps = 0
+while (steps < 8) {
+  down
+  steps = steps + 1
+}
+right
+right
+while (getColor != "red") {
+  up
+}
+var steps = 0
+while (steps < 7) {
+  down
+  steps = steps + 1
+}
+right
+right
+while (getColor != "red") {
+  down
+}
+var steps = 0
+while (steps < 7) {
+  down
+  steps = steps + 1
+}
+right
+right
+while (getColor != "red") {
+  up
+}
+var steps = 0
+while (steps < 7) {
+  up
+  steps = steps + 1
+  right
+  right
+  while (getColor != "red") {
+  down
+}
+var steps = 0
+while (steps < 7) {
+  down
+  steps = steps + 1
+}
+right
