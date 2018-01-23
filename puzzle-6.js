09417f1bc01d0b20905de3e500bf694b280d1e2d
WinstonBlack <35714947+WinstonBlack@users.noreply.github.com>
2018-01-22 21:08:28 -0800

    Update puzzle-6.js

--- puzzle-6.js
+++ puzzle-6.js
@@ -1,1 +1,22 @@
-
+right
+var firstColorSeen = getColor
+if (getColor == "red") {
+  up
+  up
+  up
+} else {
+  down
+  down
+  down
+}
+if (getColor == "blue") {
+  right
+} else {
+  left
+}
+setColor(firstColorSeen)
+if (getColor == "blue") {
+  down
+} else {
+  up
+}
