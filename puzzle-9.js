c12b897abf667e9d762deeb68a8db3c4d23c86ea
WinstonBlack <35714947+WinstonBlack@users.noreply.github.com>
2018-01-22 20:47:26 -0800

    Create puzzle-9.js

---
+++ puzzle-9.js
@@ -0,0 +1,32 @@
+while (getColor != "red") {
+  right
+}
+var steps = 0
+while (steps < 9) {
+  right
+  steps = steps + 1
+}
+while (getColor != "red") {
+  down
+}
+var steps = 0
+while (steps < 9) {
+  down
+  steps = steps + 1
+}
+while (getColor != "red") {
+  left
+}
+var steps = 0
+while (steps < 9) {
+  left 
+  steps = steps + 1
+}
+while (getColor != "red") {
+  up
+}
+var steps = 0
+while (steps < 9) {
+  up
+  steps = steps + 1
+}
